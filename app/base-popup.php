630ed81ab884943b8c24e77c27b5e93a4a902107
danielwang <danielwang130@gmail.com>
2018-04-23 21:43:19 +1000

    create buckets

---
+++ app/base-popup.php
@@ -0,0 +1,33 @@
+<!-- ******************
+*** T1 template ***
+******************  -->
+<?php
+  require_once 'ti.php';
+  include_once '_head.php'; ?>
+
+   <?php emptyblock('page-notification') ?>
+<?php startblock('popup-top');?>
+<?php endblock()?>
+<!-- ************  page header ************ -->
+<?php startblock('page-header');?>
+    <header id="gel-header" role="banner">
+        <div class="container">
+          <?php startblock('page-tabs') ?>
+          <ul class="page-tabs nav" role="tablist">
+            <li class="nav-item"><a class="nav-link active" id="tab-1" data-toggle="tab" href="#tab1" role="tab" aria-controls="Tab one" aria-selected="true">Tab one</a></li>
+            <li class="nav-item"><a class="nav-link" id="tab-2" data-toggle="tab" href="#tab2" role="tab" aria-controls="Tab two" aria-selected="">Tab two</a></li>
+            <li class="nav-item"><a class="nav-link " id="tab-3" data-toggle="tab" href="#tab3" role="tab" aria-controls="Tab three" aria-selected="">Tab Three</a></li>
+            <li class="nav-item"><a class="nav-link " id="tab-4" data-toggle="tab" href="#tab4" role="tab" aria-controls="Tab four" aria-selected="">Tab four</a></li>
+          </ul>
+          <?php endblock() ?>
+    </header>
+<?php endblock()?>
+
+<!-- ************  page body ************ -->
+		<main id="gel-main" role="main">
+			<section class="container">
+				<?php startblock('page-body')?>
+				<?php endblock()?>
+			</section>
+		</main>
+<?php include '_foot.html';?>
---
+++ app/pages/applicant-card.php
@@ -0,0 +1,214 @@
+<?php $pageTitle = "Applicant card"; include '../base-popup.php';?>
+
+<?php startblock('popup-top');?>
+<section id="popup-top" class="bg-navy text-white">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <h2 class="text-white">Victoria Johnson</h2>
+        </div>
+        <div class="col-md-6">
+          <div class="btn-group btn-group-toggle" data-toggle="buttons">
+            <label class="btn btn-sm btn-ctrl active">
+              <input type="radio" name="options-sm" id="option1" autocomplete="off" checked="checked">
+              Active
+            </label>
+            <label class="btn btn-sm btn-ctrl">
+              <input type="radio" name="options-sm" id="option2" autocomplete="off">
+              Inactive
+            </label>
+          </div>
+          <fieldset class="d-inline">
+            <div class="bucket bucket-green bucket-lg">
+              <input type="radio" name="radio-b-l" id="radio-b-l-1" value="option-b-l">
+              <label for="radio-b-l-1">
+                <span class="sr-only">
+                  Green</span>
+              </label>
+            </div>
+            <div class="bucket bucket-orange bucket-lg">
+              <input type="radio" name="radio-b-l" id="radio-b-l-2" value="option-b-l">
+              <label for="radio-b-l-2">
+                <span class="sr-only">
+                  Orange</span>
+              </label>
+            </div>
+            <div class="bucket bucket-red bucket-lg">
+              <input type="radio" name="radio-b-l" id="radio-b-l-3" value="option-b-l">
+              <label for="radio-b-l-3">
+                <span class="sr-only">
+                  Red</span>
+              </label>
+            </div>
+          </fieldset>
+        </div>
+      </div>
+      <div class="w-100 mt-3"></div>
+      <dl class="row">
+        <div class="col-6 col-md-4 col-lg-2">
+          <dt>Gender</dt>
+          <dd>Male</dd>
+        </div>
+        <div class="col-6 col-md-4 col-lg-2">
+          <dt>Phone</dt>
+          <dd>+61 4123 111 111</dd>
+        </div>
+        <div class="col-6 col-md-4 col-lg-2">
+          <dt>Address</dt>
+          <dd>91 William Street, Melbourne, VIC, Australia</dd>
+        </div>
+        <div class="col-6 col-md-4 col-lg-2">
+          <dt>Employment status</dt>
+          <dd><a href="#"> Never</a></dd>
+        </div>
+        <div class="col-6 col-md-4 col-lg-2">
+          <dt>Flags</dt>
+          <dd>Male</dd>
+        </div>
+        <div class="col-6 col-md-4 col-lg-2">
+          <dt>Number</dt>
+          <dd>10923445</dd>
+        </div>
+        <div class="col-6 col-md-4 col-lg-2">
+          <dt>Gender</dt>
+          <dd>Male</dd>
+        </div>
+        <div class="col-6 col-md-4 col-lg-2">
+          <dt>Phone</dt>
+          <dd>+61 4123 111 111</dd>
+        </div>
+        <div class="col-6 col-md-4 col-lg-2">
+          <dt>Address</dt>
+          <dd>91 William Street, Melbourne, VIC, Australia</dd>
+        </div>
+        <div class="col-6 col-md-4 col-lg-2">
+          <dt>Employment status</dt>
+          <dd><a href="#"> Never</a></dd>
+        </div>
+      </dl></div>
+</section>
+<?php endblock()?>
+
+<?php startblock('page-body');?>
+<table class="table table-striped table-hover">
+  <thead class="thead-dark">
+    <tr>
+      <th></th>
+      <th>Submitted</th>
+      <th>Status</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Gender</th>
+      <th>Mobile</th>
+      <th>Email</th>
+    </tr>
+  </thead>
+  <tbody>
+        <tr>
+      <th>
+        <span class="checkbox checkbox-default">
+          <input type="checkbox" id="checkbox-1">
+          <label for="checkbox-1"></label>
+        </span>
+      </th>
+      <td>17 Nov 2017</td>
+      <td>
+        <a href="#">Not shortlisted</a>
+      </td>
+      <td>
+        <a href="#">Achamyeleh</a>
+      </td>
+      <td>
+        <a href="#">Berhanu</a>
+      </td>
+      <td>M</td>
+      <td>+61 411 111 036</td>
+      <td>Achamyeleh.Berhanu@dreamworld.com</td>
+    </tr>
+        <tr>
+      <th>
+        <span class="checkbox checkbox-default">
+          <input type="checkbox" id="checkbox-2">
+          <label for="checkbox-2"></label>
+        </span>
+      </th>
+      <td>17 Nov 2017</td>
+      <td>
+        <a href="#">Not shortlisted</a>
+      </td>
+      <td>
+        <a href="#">Achamyeleh</a>
+      </td>
+      <td>
+        <a href="#">Berhanu</a>
+      </td>
+      <td>M</td>
+      <td>+61 411 111 037</td>
+      <td>Achamyeleh.Berhanu@dreamworld.com</td>
+    </tr>
+        <tr>
+      <th>
+        <span class="checkbox checkbox-default">
+          <input type="checkbox" id="checkbox-3">
+          <label for="checkbox-3"></label>
+        </span>
+      </th>
+      <td>17 Nov 2017</td>
+      <td>
+        <a href="#">Not shortlisted</a>
+      </td>
+      <td>
+        <a href="#">Achamyeleh</a>
+      </td>
+      <td>
+        <a href="#">Berhanu</a>
+      </td>
+      <td>M</td>
+      <td>+61 411 111 038</td>
+      <td>Achamyeleh.Berhanu@dreamworld.com</td>
+    </tr>
+        <tr>
+      <th>
+        <span class="checkbox checkbox-default">
+          <input type="checkbox" id="checkbox-4">
+          <label for="checkbox-4"></label>
+        </span>
+      </th>
+      <td>17 Nov 2017</td>
+      <td>
+        <a href="#">Not shortlisted</a>
+      </td>
+      <td>
+        <a href="#">Achamyeleh</a>
+      </td>
+      <td>
+        <a href="#">Berhanu</a>
+      </td>
+      <td>M</td>
+      <td>+61 411 111 039</td>
+      <td>Achamyeleh.Berhanu@dreamworld.com</td>
+    </tr>
+        <tr>
+      <th>
+        <span class="checkbox checkbox-default">
+          <input type="checkbox" id="checkbox-5">
+          <label for="checkbox-5"></label>
+        </span>
+      </th>
+      <td>17 Nov 2017</td>
+      <td>
+        <a href="#">Not shortlisted</a>
+      </td>
+      <td>
+        <a href="#">Achamyeleh</a>
+      </td>
+      <td>
+        <a href="#">Berhanu</a>
+      </td>
+      <td>M</td>
+      <td>+61 411 111 040</td>
+      <td>Achamyeleh.Berhanu@dreamworld.com</td>
+    </tr>
+      </tbody>
+</table>
+<?php endblock()?>
