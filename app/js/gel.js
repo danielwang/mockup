ff07531c5ad0727c796fc2e2467e7c7799c82544
danielwang <danielwang130@gmail.com>
2018-04-24 22:01:56 +1000

    remember menu state in local storage

--- app/js/gel.js
+++ app/js/gel.js
@@ -1,24 +1,46 @@
+var menu = document.getElementById('gel-menu');
+var menuIcons = document.getElementsByClassName('nav-toggle');
+
 document.addEventListener('DOMContentLoaded', () => {
+  // read menu state from localStorage
+  menuState();
 
   // sliding menu toggle
-  var menu = document.getElementById('gel-menu');
-	var menuIcons = document.getElementsByClassName('nav-toggle');
-
   for (var i=0;i<menuIcons.length; i++) {
       menuIcons[i].addEventListener('click', () => {
     		menu.classList.toggle('open');
         if (menu.classList == ""){
           setAriaExpanded('false');
+          setMenuState(false);
         }else {
           setAriaExpanded('true');
+          setMenuState(true);
         }
     	});
   }
-  // screen reader accessibility
-  function setAriaExpanded(val){
-    for (var i=0;i<menuIcons.length; i++) {
-      menuIcons[i].setAttribute('aria-expanded', val);
-    }
-  }
 
 });
+
+// screen reader accessibility
+function setAriaExpanded(val){
+  for (var i=0;i<menuIcons.length; i++) {
+    menuIcons[i].setAttribute('aria-expanded', val);
+  }
+}
+
+//  Store menu state in local storage 
+function menuState() {
+    // if local storage supported, stores display preferrence
+    if (window.localStorage) {
+        var menuState = localStorage.getItem("menuState");
+        if (menuState == 'true') {
+            menu.classList.add('open');
+        } else {
+            setMenuState(false);
+        }
+    }
+}
+
+function setMenuState(val) {
+    localStorage.setItem("menuState", val); // store the value in local browser
+}
