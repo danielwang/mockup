80bfe70a914ad34636d8002a0ef3533839dbb89f
danielwang <danielwang130@gmail.com>
2018-05-07 16:15:26 +1000

    scoped v3.1.1

--- app/_head.php
+++ app/_head.php
@@ -2,7 +2,7 @@
   include_once('functions.php');
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="gel">
   <head>
       <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
