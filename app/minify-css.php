a3f7d94b4ff8bb1b3dff26bb544c7d634b096b37
danielwang <danielwang130@gmail.com>
2018-07-09 12:12:10 +1000

    minified css files

--- app/minify-css.php
+++ app/minify-css.php
@@ -1,7 +1,8 @@
  <?php
   include_once("vendor/minifier.php");
   $css = array(
-      "css/gel-scoped.css" => "css/gel-scoped.min.css"
+      "css/gel-scoped.css" => "css/gel-scoped.min.css",
+      "css/gel.css" => "css/gel.min.css"
   );
   minifyCSS($css);
 ?>
