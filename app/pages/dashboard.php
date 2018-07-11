ae02f29eb47a6635aa7e05aef913f395d3f3c342
danielwang <danielwang130@gmail.com>
2018-07-11 17:10:10 +1000

    created recruiter dash

---
+++ app/pages/dashboard.php
@@ -0,0 +1,14 @@
+<?php $pageTitle = "Recruitment Dashboard"; $parent = "adm"; include '../base-t1.php';?>
+
+<?php //startblock('page-notification') ?>
+    <!-- <div id="gel-notification" class="gel-notification-error">
+      <i class="fa fa-exclamation-triangle" aria-hidden="true"></i> An error occurred while importing Job file last night. One or more mandatory fields were not provided.
+      <button class="btn btn-sm btn-round">Find more info</button>
+      <button href="#" type="button" class="close"><i class="fa fa-times"></i></button>
+    </div> -->
+<?php // endblock() ?>
+
+<?php startblock('page-body');?>
+
+
+<?php endblock()?>
