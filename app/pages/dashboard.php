45f8d1942bda752cbf7d09f3f6608671b1d34d85
danielwang <danielwang130@gmail.com>
2018-07-18 16:26:55 +1000

    finalised the footer

--- app/pages/dashboard.php
+++ app/pages/dashboard.php
@@ -19,101 +19,36 @@
 <?php endblock() ?>
 
 <?php startblock('page-body');?>
-<div class="row">
-  <div class="col-sm-2">
-  </div>
-  <div class="col-sm-10">
-    <div class="filter">
-      <span class="circle">
-        <span>18</span>
-      </span>
-      <span>Interview</span>
-    </div>
-  </div>
-</div>
-<table class="table table-striped table-hover">
-      <thead class="thead-dark">
-        <tr>
-          <th>Applicants</th>
-          <th>Position</th>
-          <th>Number</th>
-          <th>Status</th>
-          <th>Hiring Manager</th>
-          <th>Positions</th>
-          <th>Vacancies</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td></td>
-          <td>
-            <a href="#"><i class="gel-icon-info-pointer"></i></a> <a href="#">International Consultant on Early Childhood Development(National ECD Scheme)</a>
-          </td>
-          <td>942130</td>
-          <td>Approved to advertise</td>
-          <td></td>
-          <td>
-            1
-          </td>
-          <td>
-            2
-          </td>
-          <td><a href="#">No offer</a></td>
-        </tr>
-      </tbody>
-    </table>
+
 <div class="card">
   <div class="row">
     <div class="col-2 col-lg-1 text-center">
       <i class="gel-icon-user gel-icon-2x"></i> <span class="text-20">12</span>
     </div>
-    <div class="col-10 col-lg-11">
-      <!-- <p><span class="badge badge-primary"></span></p> -->
+    <div class="col-10 col-lg-4">
       <h4><a href="#"><i class="gel-icon-info-pointer"></i></a> <a href="#">Customer Sales &amp; Service Consultant</a></h4>
 
         <div class="d-flex flex-column flex-lg-row justify-content-lg-between">
-            <span class=""><sup class="text-black-50">Status:</sup><br/> <span>Approved to advertise</span></span>
-            <span class=""><sup class="text-black-50">Job Number:</sup> <br/><span>942130</span></span>
-            <span class=""><sup class="text-black-50">Hiring Manager:</sup> <br/><span>Daniel Wang</span></span>
+            <span>Approved to advertise</span>
+            <span>#J9827364</span></div>
+        <!--     <span class=""><sup class="text-black-50">Hiring Manager:</sup> <br/><span>Daniel Wang</span></span>
             <span class=""><sup class="text-black-50">Positions:</sup> <br/><span> 6</span></span>
             <span class=""><sup class="text-black-50">Vacancies:</sup> <br/><span> 10</span></span>
-        </div>
-        <ul class="list-unstyled">
-            <li>2 new applications
-                <a href="#1">View</a>
-            </li>
-            <li>5 applications in Offer approval commenced for more than 3 days<text>.</text>
-                <a href="#">View</a>
-            </li>
-        </ul>
+         -->
       </div>
-  </div>
-  <hr>
-  <div class="row">
-    <div class="col-2 col-lg-1 text-center">
-      <i class="gel-icon-user gel-icon-2x"></i> <span class="text-20">12</span>
+    <div class="col-2 col-lg-2">Aaron Hardy</div>
+    <div class="col-2 col-lg-1">5</div>
+    <div class="col-2 col-lg-1">9</div>
+    <div class="col-2 col-lg-3">
+      <ul class="list-unstyled">
+          <li>2 new applications
+              <a href="#1">View</a>
+          </li>
+          <li>5 applications in Offer approval commenced for more than 3 days<text>.</text>
+              <a href="#">View</a>
+          </li>
+      </ul>
     </div>
-    <div class="col-10 col-lg-11">
-      <!-- <p><span class="badge badge-primary"></span></p> -->
-      <h4><a href="#"><i class="gel-icon-info-pointer"></i></a> <a href="#">Customer Sales &amp; Service Consultant</a></h4>
-
-        <div class="d-flex flex-column flex-lg-row justify-content-lg-between">
-            <span class="d-flex justify-content-sm-between"><sup class="text-black-50">Status:</sup><br/> <span>Approved to advertise</span></span>
-            <span class="d-flex justify-content-sm-between"><sup class="text-black-50">Job Number:</sup> <br/><span>942130</span></span>
-            <span class="d-flex justify-content-sm-between"><sup class="text-black-50">Hiring Manager:</sup> <br/><span>Daniel Wang</span></span>
-            <span class="d-flex justify-content-sm-between"><sup class="text-black-50">Positions:</sup> <br/><span> 6</span></span>
-            <span class="d-flex justify-content-sm-between"><sup class="text-black-50">Vacancies:</sup> <br/><span> 10</span></span>
-        </div>
-        <ul class="list-unstyled">
-            <li>2 new applications
-                <a href="#1">View</a>
-            </li>
-            <li>5 applications in Offer approval commenced for more than 3 days<text>.</text>
-                <a href="#">View</a>
-            </li>
-        </ul>
-      </div>
   </div>
 </div>
 <?php endblock()?>
