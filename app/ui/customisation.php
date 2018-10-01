3426dc37bf35df594940a3a0d676b40fd061604e
Daniel Wang <daniel.wang@Daniels-MacBook.local>
2018-10-01 10:11:01 +1000

    fixed the z-index for sticky footer

---
+++ app/ui/customisation.php
@@ -0,0 +1,12 @@
+<?php $pageTitle = "Customisation"; $parent = "ui"; $hasSubPage = false; include '../base-ui.php'; ?>
+
+<?php startblock('page-tagline');?>
+
+<?php endblock()?>
+
+<?php startblock('page-body');?>
+<div class="card">
+<div class="col-md-6"></div>
+<div class="col-md-6"></div>
+</div>
+<?php endblock()?>
