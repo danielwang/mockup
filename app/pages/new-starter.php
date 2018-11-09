9ad8c9f533ffa9849cbac967d0103c88bb8a484c
danielwang <danielwang130@gmail.com>
2018-11-09 21:36:46 +1100

    xxl container witdth to 1560

--- app/pages/new-starter.php
+++ app/pages/new-starter.php
@@ -125,7 +125,7 @@
         </thead>
         <tbody>
         <?php for ($c =1; $c <= 4; $c++) {?>
-          <tr><td scope="row" class="section-heading" colspan="5"> <h5 class="mb-0">James's Day <?php echo $c ?></h5></td></tr>
+          <tr><td scope="row" class="section-heading" colspan="5"> <h5 class="mb-0 float-left mt-1">James's Day <?php echo $c ?></h5> <button class="float-right btn btn-sm btn-ctrl"><i class="gel-icon-sm gel-icon-add"></i> Add</button></td></tr>
           <tr>
             <th scope="row"><a href="#">Get your gears set up</a></th>
             <td data-title="Assigned to">Emily Employee</td>
