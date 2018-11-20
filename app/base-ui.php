25666e034a10a5afc59640b86f6ccb3ff95995a0
danielwang <danielwang130@gmail.com>
2018-11-21 09:08:50 +1100

    gel prep

--- app/base-ui.php
+++ app/base-ui.php
@@ -11,7 +11,7 @@
     <div class="container">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item"><span class="nav-toggle"><i class="gel-icon-menu gel-icon-2x"></i></span></li>
-        <li class="nav-item"><a class="logo" href="<?php echo ($resURL) ?>"><?php startblock('logo') ?> <img alt="logo" src="<?php echo ($resURL) ?>site-images/gel-logo.png" width="60"/><?php endblock() ?> <sub class="badge badge-info"><small>WIP</small></sub></a></li>
+        <li class="nav-item"><a title="PageUp Global Experience Language" class="logo" href="<?php echo ($resURL) ?>"><?php startblock('logo') ?> <img alt="PageUp Global Experience Language" src="<?php echo ($resURL) ?>site-images/gel-logo.png" width="60"/><?php endblock() ?> <sub class="badge badge-info"><small>WIP</small></sub></a></li>
       </ul>
       <ul class="navbar-nav">
         <li class="nav-item"><a class="nav-link" target="_blank" data-toggle="tooltip" data-placement="bottom" data-original-title="Slack" href="https://pageup.slack.com/messages/C2X8J0V70/team/" title="Slack">
@@ -28,16 +28,16 @@
   </nav>
   <!-- ************ menu ************  -->
   <aside id="gel-menu" role='menu'>
-    <span class="nav-toggle close"><i class="gel-icon-close-o gel-icon-2x"></i></span>
+    <a href="#" aria-label="Close menu" class="nav-toggle close" aria-expanded="false"><i aria-hidden="true" class="gel-icon-close-o gel-icon-2x"></i></a>
 
-    <a href="<?php echo ($resURL) ?>" class="logo"> <?php startblock('logo') ?><img alt="logo" src="<?php echo ($resURL) ?>site-images/gel-logo.png" width="75"/><?php endblock() ?></a>
+    <a href="<?php echo ($resURL) ?>" class="logo"> <?php startblock('logo') ?><img alt="logo" src="<?php echo ($resURL) ?>site-images/pageup-gel-logo.png" width="120"/><?php endblock() ?></a>
 
     <menu class="scroll">
       <?php include ("_partials/menu/ui.html");?>
     </menu>
   </aside>
 <!-- page header -->
-  <header id="gel-header" role="banner">
+  <header id="gel-header" class="py-lg-4" role="banner">
     <div class="container">
       <div class="row">
         <div class="col-lg-8 order-2 order-lg-1">
