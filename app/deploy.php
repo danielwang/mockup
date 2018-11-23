2579f64a152c2220c3b8f25ab283ff13e79ee085
danielwang <danielwang130@gmail.com>
2018-11-23 16:34:42 +1100

    i button a11y

--- app/deploy.php
+++ app/deploy.php
@@ -103,7 +103,6 @@ minifyCSS($css);
 // copy files to dist
 copy('css/gel.css', '../dist/css/gel.css');
 copy('css/gel.min.css', '../dist/css/gel.min.css');
-copy('css/gel-site.css', '../dist/css/gel-site.css');
 copy('css/gel-scoped.css', '../dist/css/gel-scoped.css');
 copy('css/gel-scoped.min.css', '../dist/css/gel-scoped.min.css');
 copy('js/gel.js', '../dist/js/gel.js');
@@ -114,6 +113,7 @@ copy('css/icons/gel-icon.woff', '../dist/css/icons/gel-icon.woff');
 //copy files to docs
 copy('js/gel.js', '../docs/js/gel.js');
 copy('css/gel.css', '../docs/css/gel.css');
+copy('css/gel-site.css', '../docs/css/gel-site.css');
 copy('css/icons/gel-icon.svg', '../docs/css/icons/gel-icon.svg');
 copy('css/icons/gel-icon.ttf', '../docs/css/icons/gel-icon.ttf');
 copy('css/icons/gel-icon.woff', '../docs/css/icons/gel-icon.woff');
