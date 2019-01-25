42e3f1c388757da2cee2ce307cbaa2b2ecebdc9c
danielwang <danielwang130@gmail.com>
2019-01-25 22:53:23 +1100

    focus state on toggles

--- app/ui/customization.php
+++ app/ui/customization.php
@@ -18,57 +18,57 @@
     <section class="bg-white p-5 rounded">
       <h1 class="text-success mb-5">Do's</h1>
       <ul class="timeline">
-      <li class="timeline-item">
-        <div class="timeline-marker"></div>
-        <div class="timeline-content">
-          <h4 class="timeline-title">Native HTML tags, such as h1, a, p, strong etc</h4>
-          <p>HTML gives meaning to content so that browsers and devices can then give that meaning to a user.</p>
-          <ul><li>If it's a heading use a heading.</li></ul>
-        </div>
-      </li>
-      <li class="timeline-item">
-        <div class="timeline-marker"></div>
-        <div class="timeline-content">
-          <h4 class="timeline-title">Leverage GEL utility classes</h4>
-          <p>Nullam vel sem. Nullam vel sem. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Donec vitae sapien ut libero venenatis faucibus. ullam dictum felis eu pede
-            mollis pretium. Pellentesque ut neque.
-          </p>
-        </div>
-      </li>
-      <li class="timeline-item">
-        <div class="timeline-marker"></div>
-        <div class="timeline-content">
-          <h4 class="timeline-title">Use GEL font icons</h4>
-          <p>Nullam vel sem. Nullam vel sem. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Donec vitae sapien ut libero venenatis faucibus. ullam dictum felis eu pede
-            mollis pretium. Pellentesque ut neque.
-          </p>
-        </div>
-      </li>
-      <li class="timeline-item">
-        <div class="timeline-marker"></div>
-        <div class="timeline-content">
-          <h4 class="timeline-title">Use GEL colours</h4>
-          <p>Nullam vel sem. Nullam vel sem. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Donec vitae sapien ut libero venenatis faucibus. ullam dictum felis eu pede
-            mollis pretium. Pellentesque ut neque.
-          </p>
-        </div>
-      </li>
-      <li class="timeline-item">
-        <div class="timeline-marker"></div>
-        <div class="timeline-content">
-          <h4 class="timeline-title">Write good alt text for your images</h4>
-          <p>Describe all of the elements that explain what’s happening in the image, rather than just setting the alt text to be something like, “photograph"
-          </p>
-        </div>
-      </li>
-      <li class="timeline-item">
-        <div class="timeline-marker"></div>
-        <div class="timeline-content">
-          <h4 class="timeline-title">Make links descriptive</h4>
-          <p>Assistive technology is able to find all links on a page and present them in various forms, but these links are rather useless if it’s a long list of links that are just the text “click here.”
-          </p>
-        </div>
-      </li>
+        <li class="timeline-item">
+          <div class="timeline-marker"></div>
+          <div class="timeline-content">
+            <h4 class="timeline-title">Native HTML tags, such as h1, a, p, strong etc</h4>
+            <p>HTML gives meaning to content so that browsers and devices can then give that meaning to a user.</p>
+            <ul><li>If it's a heading use a heading.</li></ul>
+          </div>
+        </li>
+        <li class="timeline-item">
+          <div class="timeline-marker"></div>
+          <div class="timeline-content">
+            <h4 class="timeline-title">Leverage GEL utility classes</h4>
+            <p>Nullam vel sem. Nullam vel sem. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Donec vitae sapien ut libero venenatis faucibus. ullam dictum felis eu pede
+              mollis pretium. Pellentesque ut neque.
+            </p>
+          </div>
+        </li>
+        <li class="timeline-item">
+          <div class="timeline-marker"></div>
+          <div class="timeline-content">
+            <h4 class="timeline-title">Use GEL font icons</h4>
+            <p>Nullam vel sem. Nullam vel sem. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Donec vitae sapien ut libero venenatis faucibus. ullam dictum felis eu pede
+              mollis pretium. Pellentesque ut neque.
+            </p>
+          </div>
+        </li>
+        <li class="timeline-item">
+          <div class="timeline-marker"></div>
+          <div class="timeline-content">
+            <h4 class="timeline-title">Use GEL colours</h4>
+            <p>Nullam vel sem. Nullam vel sem. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Donec vitae sapien ut libero venenatis faucibus. ullam dictum felis eu pede
+              mollis pretium. Pellentesque ut neque.
+            </p>
+          </div>
+        </li>
+        <li class="timeline-item">
+          <div class="timeline-marker"></div>
+          <div class="timeline-content">
+            <h4 class="timeline-title">Write good alt text for your images</h4>
+            <p>Describe all of the elements that explain what’s happening in the image, rather than just setting the alt text to be something like, “photograph"
+            </p>
+          </div>
+        </li>
+        <li class="timeline-item">
+          <div class="timeline-marker"></div>
+          <div class="timeline-content">
+            <h4 class="timeline-title">Make links descriptive</h4>
+            <p>Assistive technology is able to find all links on a page and present them in various forms, but these links are rather useless if it’s a long list of links that are just the text “click here.”
+            </p>
+          </div>
+        </li>
       </ul>
     </section>
   </div>
@@ -142,47 +142,5 @@
     </section>
   </div>
 </div>
-<style>
-.timeline-marker:before{
-  background: transparent;
-  color: #10a746;
-  content: "\e982";
-  font-family: 'gel-icon';
-  font-size: 32px;
-  top: 0;
-  left: -12px;
-}
-.timeline-marker:after {
-    content: "";
-    width: 1px;
-    background: transparent;
-    display: block;
-    position: absolute;
-    top: 24px;
-    bottom: 0;
-    left: 6px;
-    border-left: 1px dashed #10a746;
-}
 
-.bad .timeline-marker:before{
-  background: transparent;
-  color: #e81700;
-  content: "\e910";
-  font-family: 'gel-icon';
-  font-size: 32px;
-  top: 0;
-  left: -12px;
-}
-.bad .timeline-marker:after {
-    content: "";
-    width: 1px;
-    background: transparent;
-    display: block;
-    position: absolute;
-    top: 24px;
-    bottom: 0;
-    left: 6px;
-    border-left: 1px dashed #e81700;
-}
-</style>
 <?php endblock()?>
